b2c8ec1daf69f2eff0e6b972e06ea387a97b2f09
yeowon <dudnjsckrgo@gmail.com>
2024-09-20 12:30:09 +0900

    프로젝트 생성

---
+++ src/main/java/org/tripleyoung/reward/RewardBackendApplication.java
@@ -0,0 +1,13 @@
+package org.tripleyoung.reward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RewardBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tripleyoung/reward/RewardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tripleyoung.reward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
